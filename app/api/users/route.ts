a85f31395539ed86ab2754ed8849042f1dc16089
andbudha <andreibartov@gmail.com>
2024-11-21 22:16:24 +0100

    posting new user request added

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,3 +5,9 @@ export async function GET() {
   const users = await prisma.user.findMany();
   return NextResponse.json(users);
 }
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const user = await prisma.user.create({ data: body });
+  return NextResponse.json(user);
+}
