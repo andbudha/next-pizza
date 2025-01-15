eb3be225dd079fa5f269cb38f217c5cbd16cb562
andbudha <andreibartov@gmail.com>
2025-01-15 11:17:27 +0100

    prisma schema&prisma constants updated

--- prisma/constants.ts
+++ prisma/constants.ts
@@ -8,90 +8,162 @@ export const categories = [
 
 export const ingredients = [
   {
+    id: 1,
     name: 'Tomato',
     imageUrl:
       'https://panettamercato.com.au/wp-content/uploads/2021/07/Tomatoes-Round-Kg-Panetta-Mercato.jpeg',
     price: 0.5,
   },
   {
+    id: 2,
     name: 'Mozzarella',
     imageUrl: 'https://www.laves.niedersachsen.de/assets/image/736/190470',
     price: 0.5,
   },
   {
+    id: 3,
     name: 'Pepperoni',
     imageUrl:
       'https://media.istockphoto.com/id/169943249/de/foto/haufen-von-pepperoni.jpg?s=612x612&w=0&k=20&c=S10tb-tAJQAxRgjpbEYKqjFA8odt1DyFABaaGm2jGVY=',
     price: 1,
   },
   {
+    id: 4,
     name: 'Bacon',
     imageUrl:
       'https://clickandgrill.de/media/catalog/product/cache/b6097499a04a19b2b148a9cbfe7b1e5d/b/a/bacon.png',
     price: 1,
   },
   {
+    id: 5,
     name: 'Cheddar',
     imageUrl: 'https://m.media-amazon.com/images/I/41RLHOUIQGL.jpg',
     price: 1,
   },
   {
+    id: 6,
     name: 'Eggs',
     imageUrl:
       'https://static.scientificamerican.com/sciam/cache/file/62936F67-7783-4949-A047CD417758AE9D_source.jpg?crop=1%3A1%2Csmart&w=1000',
     price: 1.5,
   },
   {
+    id: 7,
     name: 'Mushrooms',
     imageUrl:
       'https://images.cdn.smartandfinal.com/detail/e41419e12f697d5e487c97767766d32d7bc02a68.jpg',
     price: 1.5,
   },
   {
+    id: 8,
     name: 'Spinach',
     imageUrl:
       'https://www.heddensofwoodtown.co.uk/wp-content/uploads/2020/05/spinach_opt.jpg',
     price: 1.5,
   },
   {
+    id: 9,
     name: 'Feta',
     imageUrl: 'https://glycemic-index.net/images/udYCjL4S7S.webp',
     price: 2,
   },
   {
+    id: 10,
     name: 'Ham',
     imageUrl:
       'https://exclusivelyfood.co.za/wp-content/uploads/2022/03/HEINZ-SMOKED-FARM-HAM-250g.jpg',
     price: 2,
   },
   {
+    id: 11,
     name: 'Basil',
     imageUrl:
       'https://www.veggycation.com.au/siteassets/veggycationvegetable/basil.jpg',
     price: 0.5,
   },
   {
+    id: 12,
     name: 'BBQ Sauce',
     imageUrl:
       'https://crawfordoutdoor.com/wp-content/uploads/2021/05/35987-4.jpg',
     price: 0.5,
   },
   {
+    id: 13,
     name: 'Onions',
     imageUrl:
       'https://produits.bienmanger.com/36700-0w470h470_Organic_Red_Onion_From_Italy.jpg',
     price: 0.5,
   },
   {
+    id: 14,
     name: 'Olives',
     imageUrl:
       'https://kontos.com/wp-content/uploads/2022/12/Kalamata-Olives.png',
     price: 0.5,
   },
   {
+    id: 15,
     name: 'Pineapple',
     imageUrl:
       'https://static.wixstatic.com/media/d612d2_447e30fbdf9241819d1008a9bdd97eee~mv2.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/Pineapple2.jpg',
     price: 1.5,
+  },
+];
+
+export const products = [
+  {
+    name: 'Bacon and Cheddar Omelette',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:233x233/0193bb3fdb8b70bb82bd1eb2d01bac5f.avif',
+    categoryId: 2,
+  },
+  {
+    name: 'Mushroom and Swiss Omelette',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:233x233/0193bb3fdb8b70bb82bd1eb2d01bac5f.avif',
+    categoryId: 2,
+  },
+  {
+    name: 'Spinach and Feta Omelette',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:233x233/0193bb3fdb8b70bb82bd1eb2d01bac5f.avif',
+    categoryId: 2,
+  },
+  {
+    name: 'Caprese Omelette',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:233x233/0193bb3fdb8b70bb82bd1eb2d01bac5f.avif',
+    categoryId: 2,
+  },
+  {
+    name: 'Prosciutto and Arugula Omelette',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:233x233/0193bb3fdb8b70bb82bd1eb2d01bac5f.avif',
+    categoryId: 2,
+  },
+  {
+    name: 'Smoked Salmon and Cream Cheese Omelette',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:233x233/0193bb3fdb8b70bb82bd1eb2d01bac5f.avif',
+    categoryId: 2,
+  },
+  {
+    name: 'Americano',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:233x233/11ee7d5f243edf28a95820f2a40aacc4.avif',
+    categoryId: 3,
+  },
+  {
+    name: 'Latte',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:233x233/11ee7d5f24b752499d31c5bdfdd3f674.avif',
+    categoryId: 3,
+  },
+  {
+    name: 'Cappuccino',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:233x233/11ee7d5f22cf7f75aa4cd07e88c36e0c.avif',
+    categoryId: 3,
   },
 ];
