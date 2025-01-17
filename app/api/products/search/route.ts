37c553e601ebb8a479777a842c5397fa73a11918
andbudha <andreibartov@gmail.com>
2025-01-17 11:58:23 +0100

    both ingredients&products route updated

--- app/api/products/search/route.ts
+++ app/api/products/search/route.ts
@@ -2,7 +2,6 @@ import { prisma } from '@/prisma/prisma-client';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  console.log('SEARCH REQUEST:::', request.nextUrl.searchParams.get('query'));
   const query = request.nextUrl.searchParams.get('query') || '';
   const products = await prisma.product.findMany({
     where: {
