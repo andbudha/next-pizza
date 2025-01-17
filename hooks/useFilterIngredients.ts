79f61023c315e12285ee26b0b2afec11c8685c30
andbudha <andreibartov@gmail.com>
2025-01-17 10:21:44 +0100

    useFilterIngredients hook created

---
+++ hooks/useFilterIngredients.ts
@@ -0,0 +1,24 @@
+import { Api } from '@/services/api-client';
+import { Ingredient } from '@prisma/client';
+import React from 'react';
+
+type Ingredients = {
+  ingredients: Ingredient[];
+};
+export const useFilterIngredients = (): Ingredients => {
+  const [ingredients, setIngredients] = React.useState<Ingredient[]>([]);
+  React.useEffect(() => {
+    async function fetchIngredients() {
+      try {
+        const ingredients = await Api.ingredients.getIngredients();
+        setIngredients(ingredients);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchIngredients();
+  });
+
+  return { ingredients };
+};
