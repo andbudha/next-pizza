7bf5be6229978014d080eac2a3eba0f9a02a3d8f
andbudha <andreibartov@gmail.com>
2025-02-03 15:13:20 +0100

    main layout added/root layout updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,4 @@
-import { Nunito } from 'next/font/google';
-import '../globals.css';
 import { Header } from '@/components/shared';
-
-const nunito = Nunito({
-  subsets: ['cyrillic'],
-  variable: '--font-nunito',
-  weight: ['400', '500', '600', '700', '800', '900'],
-});
 
 export const metadata = {
   title: 'Next Pizza',
@@ -17,13 +9,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={nunito.className}>
-        <main className="min-h-screen">
-          <Header />
-          {children}
-        </main>
-      </body>
-    </html>
+    <main className="min-h-screen">
+      <Header />
+      {children}
+    </main>
   );
 }
